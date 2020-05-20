4735c340f94651114acf3a93d5c1c5356790c727
Jakob Lindqvist <jakob.lindqvist@chalmers.se>
2020-05-20 22:26:32 +0200

    WIP EKF

--- src/post_lin_filt/filtering.py
+++ src/post_lin_filt/filtering.py
@@ -8,31 +8,31 @@ def non_linear_kalman_filter(measurements, prior_mean, prior_cov, motion_model,
     Filters a measurement sequence using a non-linear Kalman filter.
     Args:
         measurements np.array(d, K): Measurement sequence for times 1,..., K
-        prior_mean np.array(n, 1): Prior mean for time 0
-        prior_cov np.array(n, n): Prior covariance
+        prior_mean np.array(D_x,): Prior mean for time 0
+        prior_cov np.array(D_x, D_x): Prior covariance
         motion_model: Motion model function handle
                       Takes as input x (state)
                       Returns predicted mean and Jacobian evaluated at x
-        process_noise_cov np.array(n, n): Process noise covariance
+        process_noise_cov np.array(D_x, D_x): Process noise covariance
         meas_model: Measurement model function handle
                     Takes as input x (state),
                     Returns measurement mean and Jacobian evaluated at x
-        meas_noise_cov np.array(m, m): Measurement noise covariance
+        meas_noise_cov np.array(D_y, D_y): Measurement noise covariance
 
     Returns:
-        filtered_mean np.array(n, K): Filtered estimates for times 1,..., K
-        filtered_cov np.array(n, n, K): Filter error covariance
-        pred_states np.array(n, K): Predicted estimates for times 1,..., K
-        pred_cov np.array(n, n, K): Filter error covariance
+        filtered_mean np.array(D_x, K): Filtered estimates for times 1,..., K
+        filtered_cov np.array(D_x, D_x, K): Filter error covariance
+        pred_states np.array(D_x, K): Predicted estimates for times 1,..., K
+        pred_cov np.array(D_x, D_x, K): Filter error covariance
     """
 
     K = measurements.shape[1]
-    n = prior_mean.shape[0]
+    dim_x = prior_mean.shape[0]
     # Data allocation
-    filtered_states = np.zeros(n, K)
-    filtered_cov = np.zeros(n, n, K)
-    pred_states = np.zeros(n, K)
-    pred_cov = np.zeros(n, n, K)
+    filtered_states = np.zeros((dim_x, K))
+    filtered_cov = np.zeros((dim_x, dim_x, K))
+    pred_states = np.zeros((dim_x, K))
+    pred_cov = np.zeros((dim_x, dim_x, K))
     for k in range(K):
         meas = measurements[:, k]
         # Run filter iteration
@@ -47,6 +47,7 @@ def non_linear_kalman_filter(measurements, prior_mean, prior_cov, motion_model,
         filtered_cov[:, :, k] = updated_cov
         prior_mean = updated_mean
         prior_cov = updated_cov
+    return filtered_states, filtered_cov, pred_states, pred_cov
 
 
 def _prediction(prior_mean, prior_cov, motion_model, process_noise_cov):
@@ -55,16 +56,16 @@ def _prediction(prior_mean, prior_cov, motion_model, process_noise_cov):
     using a non-linear Gaussian model.
 
     Args:
-        prior_mean np.array(n, 1): Prior mean
-        prior_cov np.array(n, n): Prior covariance
+        prior_mean np.array(D_x): Prior mean
+        prior_cov np.array(D_x, D_x): Prior covariance
         motion_model: Motion model function handle
                       Takes as input x (state)
                       Returns predicted mean and Jacobian evaluated at x
-       process_noise_cov np.array(n, n): Process noise covariance
+       process_noise_cov np.array(D_x, D_x): Process noise covariance
 
     Returns:
-       pred_mean np.array(n, n): predicted state mean
-       pred_cov np.array(n, n): predicted state covariance
+       pred_mean np.array(D_x, D_x): predicted state mean
+       pred_cov np.array(D_x, D_x): predicted state covariance
     """
     pred_state, jacobian = motion_model(prior_mean)
     pred_cov = jacobian @ prior_cov @ jacobian.T + process_noise_cov
@@ -77,20 +78,21 @@ def _update(prior_mean, prior_cov, meas, meas_model, meas_noise_cov):
     using a non-linear Gaussian model.
 
     Args:
-        prior_mean np.array(n, 1): Prior mean
-        prior_cov np.array(n, n): Prior covariance
-        meas np.array(d, 1): Measurement
+        prior_mean np.array(D_x,): Prior mean
+        prior_cov np.array(D_x, D_x): Prior covariance
+        meas np.array(d,): Measurement
         meas_model: Measurement model function handle
                     Takes as input x (state),
                     Returns measurement mean and Jacobian evaluated at x
-        meas_noise_cov np.array(m, m): Measurement noise covariance
+        meas_noise_cov np.array(D_y, D_y): Measurement noise covariance
 
     Returns:
-       updated_mean np.array(n, n): updated state mean
-       updated_cov np.array(n, n): updated state covariance
+       updated_mean np.array(D_x, D_x): updated state mean
+       updated_cov np.array(D_x, D_x): updated state covariance
     """
     [meas_mean, jacobian] = meas_model(prior_mean)
     S = jacobian @ prior_cov @ jacobian.T + meas_noise_cov
+    print("S", S)
     K = prior_cov @ jacobian.T @ np.linalg.inv(S)
     updated_mean = prior_mean + K @ (meas - meas_mean)
     updated_cov = prior_cov - K @ S @ K.T
--- src/smoother.py
+++ src/smoother.py
@@ -9,7 +9,7 @@ from post_lin_filt.filtering import non_linear_kalman_filter
 def main():
     K = 600
     sampling_period = 0.1
-    pos = np.array([280, -140])
+    pos = np.array([[280], [-140]])
     sigma_r = 15
     sigma_phi = 4 * np.pi / 180
 
@@ -17,9 +17,8 @@ def main():
     meas_model = partial(range_bearing_meas, pos=pos)
     true_states, meas = gen_dummy_data(K, sampling_period, meas_model, pos, R)
     cartes_meas = to_cartesian_coords(meas, pos)
-    plot_(true_states, cartes_meas)
 
-    x_0 = np.zeros((5, 1))
+    x_0 = np.zeros((5, ))
     P_0 = np.diag(
         [10**2, 10**2, 10**2, (5 * np.pi / 180)**2, (1 * np.pi / 180)**2])
     v_scale = 0.01
@@ -31,41 +30,61 @@ def main():
     ])
     motion_model = partial(coord_turn_motion, sampling_period=sampling_period)
 
-    xs, Ps, xf, Pf, xp, Pp = non_linear_kalman_filter(meas, x_0, P_0,
-                                                      motion_model, Q,
-                                                      meas_model, R)
+    xf, Pf, xp, Pp = non_linear_kalman_filter(meas, x_0, P_0, motion_model, Q,
+                                              meas_model, R)
+    plot_(true_states, cartes_meas, xf, Pf)
 
 
-def plot_(true_states, meas):
+def plot_(true_states, meas, filtered_mean, filtered_cov):
     plt.plot(true_states[0, :], true_states[1, :])
+    plt.plot(filtered_mean[0, :], filtered_mean[1, :])
     plt.plot(meas[0, :], meas[1, :], "r*")
     plt.show()
 
 
-def range_bearing_meas(states, pos):
-    print(states.shape)
-    range_ = np.sum((states[0:1, :] - pos)**2)
-    bearing = np.arctan2(states[1, :] - pos[1], states[0, :] - pos[0])
-    meas_mean = np.array([range_, bearing]).T
-    jacobian = 0
+def range_bearing_meas(state, pos):
+    """Range bearing measurement
+
+    Args:
+        state np.array(D_x,)
+        pos np.array(D_y,)
+
+    Returns:
+        meas_mean np.array(D_y,)
+        jacobian np.array(D_y, D_x)
+    """
+
+    range_ = np.sqrt(np.sum((state[:2] - pos)**2, 0))
+    bearing = np.arctan2(state[1] - pos[1], state[0] - pos[0])
+    meas_mean = np.array([range_, bearing])
+    jacobian = np.array([[(state[0] - pos[0]) / range_,
+                          (state[1] - pos[1]) / range_, 0, 0, 0],
+                         [
+                             -(state[1] - pos[1]) / (range_**2),
+                             (state[0] - pos[0]) / (range_**2), 0, 0, 0
+                         ]])
 
     return meas_mean, jacobian
 
 
-def coord_turn_motion(state, sampling_time):
+def coord_turn_motion(state, sampling_period):
     v = state[2]
     phi = state[3]
     omega = state[4]
 
     mean = state + np.array([
-        sampling_time * v * np.cos(phi), sampling_time * v * np.sin(phi), 0,
-        sampling_time * omega, 0
+        sampling_period * v * np.cos(phi), sampling_period * v * np.sin(phi),
+        0, sampling_period * omega, 0
     ])
 
     jacobian = np.array([[
-        1, 0, sampling_time * np.cos(phi), -sampling_time * v * np.sin(phi), 0
-    ], [0, 1, sampling_time * np.sin(phi), sampling_time * v * np.cos(phi), 0],
-                         [0, 0, 1, 0, 0], [0, 0, 0, 1, sampling_time],
+        1, 0, sampling_period * np.cos(phi),
+        -sampling_period * v * np.sin(phi), 0
+    ],
+                         [
+                             0, 1, sampling_period * np.sin(phi),
+                             sampling_period * v * np.cos(phi), 0
+                         ], [0, 0, 1, 0, 0], [0, 0, 0, 1, sampling_period],
                          [0, 0, 0, 0, 1]])
     return mean, jacobian
 
@@ -76,27 +95,40 @@ def gen_dummy_data(num_samples, sampling_period, meas_model, pos, R):
     # Initial state
     initial_state = np.array([0, 0, 20, 0, omega[0]])
     # Allocate memory
-    true_states = np.zeros((initial_state.shape[0], num_samples + 1))
-    true_states[:, 0] = initial_state
+    true_states = np.zeros((num_samples + 1, initial_state.shape[0]))
+    true_states[0, :] = initial_state
     # Create true track
     for k in range(1, num_samples + 1):
-        new_state, _ = coord_turn_motion(true_states[:, k - 1],
+        new_state, _ = coord_turn_motion(true_states[k - 1, :],
                                          sampling_period)
-        true_states[:, k] = new_state
-        true_states[4, k] = omega[k]
+        true_states[k, :] = new_state
+        true_states[k, 4] = omega[k]
 
     return true_states, gen_non_lin_meas(true_states, meas_model, R)
 
 
 def gen_non_lin_meas(states, meas_model, R):
-    meas_mean, _ = meas_model(states)
-    noise = mvn.rvs(mean=np.zeros(meas_mean.shape), cov=R, size=1)
+    """Generate non-linear measurements
+
+    Args:
+        states np.array((K, D_x))
+        meas_model
+        R np.array((D_y, D_y))
+    """
+
+    # Transpose juggling to get np to apply a function row-wise
+    meas_mean, _ = meas_model(states.T)
+    meas_mean = meas_mean.T
+    num_states, meas_dim = meas_mean.shape
+    noise = mvn.rvs(mean=np.zeros((meas_dim, )), cov=R, size=num_states)
     return meas_mean + noise
 
 
 def to_cartesian_coords(meas, pos):
-    return np.array([meas[0] * np.cos(meas[1]), meas[0] * np.sin(meas[1])
-                     ]).T + pos
+    delta_x = meas[:, 0] * np.cos(meas[:, 1])
+    delta_y = meas[:, 0] * np.sin(meas[:, 1])
+    coords = np.array([delta_x, delta_y]) + pos
+    return coords
 
 
 if __name__ == "__main__":
