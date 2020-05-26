6fc1287609ad955f6cf8d2b164c08c87c6c2ad49
Jakob <jakob.lindqvist@chalmers.se>
2020-05-26 14:54:49 +0200

    WIP FHM model

--- src/post_lin_filt/meas_models/range_bearing.py
+++ src/post_lin_filt/meas_models/range_bearing.py
@@ -9,21 +9,6 @@ class RangeBearing:
         self.pos = pos
 
     def update(self, prior_mean, prior_cov, meas, meas_noise_cov):
-        """Non-linear Kalman filter update.
-        Calculates an updated mean and covariance of the state density
-        using a non-linear Gaussian model.
-
-        Args:
-            prior_mean np.array(D_x,): Prior mean
-            prior_cov np.array(D_x, D_x): Prior covariance
-            meas_noise_cov np.array(D_y, D_y): Measurement noise covariance
-            meas np.array(d,): Measurement
-
-        Returns:
-           updated_mean np.array(D_x, D_x): updated state mean
-           updated_cov np.array(D_x, D_x): updated state covariance
-        """
-
         [meas_mean, jacobian] = self.mean_and_jacobian(prior_mean)
         S = jacobian @ prior_cov @ jacobian.T + meas_noise_cov
         K = prior_cov @ jacobian.T @ np.linalg.inv(S)
