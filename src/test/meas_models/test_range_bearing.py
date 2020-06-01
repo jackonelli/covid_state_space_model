45564133ba7423e0d4b326ae22aa5c1fae15e5be
Jakob Lindqvist <jakob.lindqvist@chalmers.se>
2020-06-01 16:42:39 +0200

    Removed old EKF versions

--- src/test/meas_models/test_range_bearing.py
+++ src/test/meas_models/test_range_bearing.py
@@ -1,16 +1,16 @@
 import unittest
 from functools import partial
 import numpy as np
-from post_lin_filt.meas_models.range_bearing import RangeBearing
+from models.range_bearing import RangeBearing
 
 
 class TestRangeBearing(unittest.TestCase):
     def test_dim_single_state(self):
         pos = np.array([0, 0])
-        meas_model = RangeBearing(pos)
+        R = np.eye(2)
+        meas_model = RangeBearing(pos, R)
         state = np.ones((5, ))
-        mean, jacobian = meas_model.mean_and_jacobian(state)
+        mean = meas_model.mean(state)
         self.assertEqual(mean.shape, (2, ))
-        self.assertEqual(jacobian.shape, (2, 5))
         # Most important to check that there are no sub-arrays
         self.assertEqual(mean[0], np.sqrt(2))
